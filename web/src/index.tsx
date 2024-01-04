7d6f1ad85996e6518dbed0af6fca3210edfe7790
mlnbk <dev.milan.bako@gmail.com>
2024-01-04 20:59:42 +0100

    feat: scaffold centralized app styling

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,14 +4,38 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { Box, ChakraProvider } from '@chakra-ui/react';
+import { Box, ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      500: '#008000',
+      700: '#004d00',
+    },
+  },
+  components: {
+    // NOTE: POC for customizing components, can be removed later
+    Button: {
+      baseStyle: {
+        color: 'white',
+        backgroundColor: 'brand.500',
+        _hover: {
+          backgroundColor: 'brand.700',
+        },
+      },
+      defaultProps: {
+        colorScheme: 'brand',
+      },
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Box minH="100vh" bgGradient="linear(to-br, green.300, green.600)">
         <App />
       </Box>
