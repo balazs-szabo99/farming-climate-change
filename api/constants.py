a251dd9d637c1ba00433b9cfc7e84304c668c9a9
mlnbk <dev.milan.bako@gmail.com>
2024-01-21 13:45:56 +0100

    feat: add units to data in API
    - Added `constants.py` to store units and chart information
    - Updated `pre_process_data.py` to include units in the dataframes and JSON output
    - Refactored chart title and description to use constants

---
+++ api/constants.py
@@ -0,0 +1,46 @@
+units = {
+    "Emissions": "kilotonnes",
+    "Agricultural Land": "square kilometers",
+    "Population": "people",
+    "Cereal": "kg per hectare",
+    "Arable Land": "square kilometers per person",
+}
+
+chart_info = {
+    "emissions_and_land": {
+        "title": "Change of greenhouse gas emission and agricultural land",
+        "description": (
+            "This chart represents the change in greenhouse gas emissions and "
+            "agricultural land use over time for a selected country. The "
+            "'Emissions' data indicates the total greenhouse gas emissions in "
+            "kilotonnes, while the 'Land' data represents the total agricultural "
+            "land area in square kilometers. By visualizing these two factors "
+            "together, we can gain insights into the relationship between "
+            "agricultural practices and their impact on the environment."
+        ),
+    },
+    "emissions_and_cereal_yield": {
+        "title": "Change of greenhouse gas emission and cereal yield",
+        "description": (
+            "This chart represents the change in greenhouse gas emissions and "
+            "cereal yield over time for a selected country. The 'Emissions' "
+            "data indicates the total greenhouse gas emissions in kilotonnes, "
+            "while the 'Cereal' data represents the total cereal yield in kg "
+            "per hectare. By visualizing these two factors together, we can gain "
+            "insights into the relationship between agricultural productivity and  "
+            "environmental impact."
+        ),
+    },
+    "population_and_arable_land": {
+        "title": "Change of population and arable land",
+        "description": (
+            "This chart represents the change in population and "
+            "arable land over time for a selected country. The 'Population' "
+            "data indicates the total population, while "
+            "the 'Land' data represents the total arable land area in hectares "
+            "per person. By visualizing these two factors together, we can gain "
+            "insights into the availability of agricultural resources and "
+            "potential challenges related to food production and land use."
+        ),
+    },
+}
