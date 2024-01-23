e0292041d057003229b1cd1a77bbd81e643498f1
mlnbk <dev.milan.bako@gmail.com>
2024-01-23 14:39:10 +0100

    feat: add Dropdown component

---
+++ web/src/Components/Dropdown.tsx
@@ -0,0 +1,60 @@
+import {
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react';
+import { FC, useEffect, useRef, useState } from 'react';
+import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
+
+interface DropdownProps {
+  selected: string;
+  options: string[];
+  onChange: (selected: string) => void;
+}
+
+const Dropdown: FC<DropdownProps> = ({ selected, options, onChange }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const handleLimitChange = (selected: string) => {
+    onChange(selected);
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (!dropdownRef.current?.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <Box position="relative">
+      <Menu>
+        <MenuButton
+          as={Button}
+          rightIcon={isOpen ? <BsChevronUp /> : <BsChevronDown />}
+        >
+          {selected}
+        </MenuButton>
+        <MenuList w={'min'}>
+          {options.map((option) => (
+            <MenuItem key={option} onClick={() => handleLimitChange(option)}>
+              {option}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+    </Box>
+  );
+};
+
+export default Dropdown;
