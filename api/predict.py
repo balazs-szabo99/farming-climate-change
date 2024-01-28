2022c0071ea9ebe31bf20471d33f536a7f6336d4
mlnbk <dev.milan.bako@gmail.com>
2024-01-28 13:26:38 +0100

    feat: add prediction function
    * predict values for next next years using ARIMA

--- api/predict.py
+++ api/predict.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
 
 # Load and preprocess the data
 data = pd.read_csv("./data/greenhouse_gas_emission.csv")
@@ -24,5 +25,31 @@ data = data.dropna(subset=data.columns[1:], how="all")
 for col in data.columns[1:]:
     data[col] = pd.to_numeric(data[col])
 
-
 data.set_index("Country Name", inplace=True)
+
+
+def predict_next_5_years(series):
+    """
+    Predicts the values for the next 5 years using ARIMA model.
+
+    Args:
+      series (pandas.Series): Time series data.
+
+    Returns:
+      numpy.ndarray: Array of predicted values for the next 5 years.
+    """
+    try:
+        series.index = pd.DatetimeIndex(series.index, freq="AS-JAN")
+        model = ARIMA(series, order=(2, 0, 2))
+        model_fit = model.fit()
+        output = model_fit.forecast(steps=5)
+        return output
+    except Exception as e:
+        print(f"Failed to fit model for country: {series.name}, Error: {str(e)}")
+        return None
+
+
+predictions = data.apply(predict_next_5_years, axis=1)
+predictions.columns = predictions.columns.year
+predictions.reset_index(inplace=True)
+print(predictions.head(20))
