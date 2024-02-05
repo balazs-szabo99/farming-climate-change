04d84b3ba82191c8f46a8d736fb5416082a8db4b
balazs-szabo99 <szbalazs99@gmail.com>
2024-02-05 10:38:08 +0100

    chore: update line ending configs

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
+    'prettier/prettier': ['error', { endOfLine: 'lf' }],
   },
 };
